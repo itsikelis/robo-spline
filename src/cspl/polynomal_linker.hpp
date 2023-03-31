ceb1dadd022b9314cf6e4519a0d0ad07e4886499
Ioannis Tsikelis <tsikelis.i@protonmail.com>
2023-04-01 00:56:00 +0300

    Starting work on polynomial linker

---
+++ src/cspl/polynomal_linker.hpp
@@ -0,0 +1,14 @@
+#include <Eigen/Dense>
+
+namespace cspl
+{
+    template <unsigned int D>
+    class PolynomialTrajectory
+    {
+    public:
+        using Vec = Eigen::Matrix<CubicHermitePolynomial<2>, D, 1>;
+
+    protected:
+        Vec 
+    };
+} // namespace cspl
