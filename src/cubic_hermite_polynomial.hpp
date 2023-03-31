29506bc6e34dcd5df4aa4891b049fa614d5325cb
Ioannis Tsikelis <tsikelis.i@protonmail.com>
2023-03-31 14:37:06 +0300

    Added variable trajectory time duration.

--- src/cubic_hermite_polynomial.hpp
+++ src/cubic_hermite_polynomial.hpp
@@ -7,6 +7,17 @@ public:
 
     CubicHermitePolynomial(const Vec& initial_position, const Vec& initial_velocity, const Vec& final_position, const Vec& final_velocity)
     {
+        _duration - 1.;
+
+        _a0 = initial_position;
+        _a1 = initial_velocity;
+        _a2 = 3. * final_position - 3. * initial_position - 2. * initial_velocity - final_velocity;
+        _a3 = -2. * final_position + 2. * initial_position + initial_velocity + final_velocity;
+    }
+
+    CubicHermitePolynomial(const Vec& initial_position, const Vec& initial_velocity, const Vec& final_position, const Vec& final_velocity, double duration)
+        : _duration(duration)
+    {
         _a0 = initial_position;
         _a1 = initial_velocity;
         _a2 = 3. * final_position - 3. * initial_position - 2. * initial_velocity - final_velocity;
@@ -15,19 +26,23 @@ public:
 
     Vec position(double t)
     {
-        return _a0 + (_a1 * t) + (_a2 * t * t) + (_a3 * t * t * t);
+        double t_normalized = t / _duration;
+        return _a0 + (_a1 * t_normalized) + (_a2 * t_normalized * t_normalized) + (_a3 * t_normalized * t_normalized * t_normalized);
     }
 
     Vec velocity(double t)
     {
-        return _a1 + (2 * _a2 * t) + (3 * _a3 * t * t);
+        double t_normalized = t / _duration;
+        return _a1 + (2 * _a2 * t_normalized) + (3 * _a3 * t_normalized * t_normalized);
     };
 
     Vec acceleration(double t)
     {
-        return (2 * _a2) + (6 * _a3 * t);
+        double t_normalized = t / _duration;
+        return (2 * _a2) + (6 * _a3 * t_normalized);
     };
 
 protected:
+    double _duration; // Total trajectory duration in seconds.
     Vec _a0, _a1, _a2, _a3;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -13,20 +13,22 @@ int main()
     Eigen::Vector3d x1 = {1., 1., 0.};
     Eigen::Vector3d v1 = {1., 1., 1.};
 
-    CubicHermitePolynomial3D pol = CubicHermitePolynomial3D(x0, v0, x1, v1);
+    double duration = 5.;
+
+    auto pol = CubicHermitePolynomial3D(x0, v0, x1, v1, duration);
 
     std::cout << "Positions:" << std::endl;
-    for (double t = 0.; t <= 1.; t += 0.1) {
+    for (double t = 0.; t <= duration; t += 0.1 * duration) {
         std::cout << pol.position(t).transpose() << std::endl;
     }
 
     std::cout << "Velocities:" << std::endl;
-    for (double t = 0.; t <= 1.; t += 0.1) {
+    for (double t = 0.; t <= duration; t += 0.1 * duration) {
         std::cout << pol.velocity(t).transpose() << std::endl;
     }
 
     std::cout << "Accelerations:" << std::endl;
-    for (double t = 0.; t <= 1.; t += 0.1) {
+    for (double t = 0.; t <= duration; t += 0.1 * duration) {
         std::cout << pol.acceleration(t).transpose() << std::endl;
     }
 
