c459b52ee4676a23c4a2629957427611dc3e2466
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2023-03-31 10:34:30 +0300

    Fix polynomial

--- src/main.cpp
+++ src/main.cpp
@@ -2,31 +2,31 @@
 
 #include <iostream>
 
-class PolynomialPair {
+template <int D>
+class CubicHermitePolynomial {
 public:
-    PolynomialPair(const Eigen::Vector2d& pos0, const Eigen::Vector2d& posFin, const Eigen::Vector2d& vel0, const Eigen::Vector2d& velFin, double t0, double tFin)
-    // : _startPos(pos0), _finalPos(posFin), _startVel(vel0), _finalVel(velFin), _startTime(t0), _finalTime(tFin)
+    using Vec = Eigen::Matrix<double, D, 1>;
+
+    CubicHermitePolynomial(const Vec& initial_position, const Vec& initial_velocity, const Vec& final_position, const Vec& final_velocity)
     {
-        _a0 = pos0;
-        _a1 = vel0;
-        _a2 = (3 / std::pow(tFin, 2)) * (posFin - pos0) - (2 / tFin) * vel0 - (1 / tFin) * velFin;
-        _a3 = -(2 / std::pow(tFin, 3)) * (posFin - pos0) + (1 / std::pow(tFin, 2)) * (velFin - vel0);
-    };
+        _a0 = initial_position;
+        _a1 = initial_velocity;
+        _a2 = 3. * final_position - 3. * initial_position - 2. * initial_velocity - final_velocity;
+        _a3 = -2. * final_position + 2. * initial_position + initial_velocity + final_velocity;
+    }
 
     Eigen::Vector2d position(double t)
     {
-        return _a0 + (_a1 * t) + (_a2 * std::pow(t, 2)) + (_a3 * std::pow(t, 3));
+        return _a0 + (_a1 * t) + (_a2 * t * t) + (_a3 * t * t * t);
     };
 
     Eigen::Vector2d velocity(double t)
     {
-        return _a1 + (2 * _a2 * t) + (3 * _a3 * std::pow(t, 2));
+        return _a1 + (2 * _a2 * t) + (3 * _a3 * t * t);
     };
 
-private:
-    // Eigen::Vector2d _startPos, _finalPos, _startVel, _finalVel;
-    // double _startTime, _finalTime;
-    Eigen::Vector2d _a0, _a1, _a2, _a3;
+protected:
+    Vec _a0, _a1, _a2, _a3;
 };
 
 int main()
@@ -37,16 +37,16 @@ int main()
     Eigen::Vector2d x1 = {1., 1.};
     Eigen::Vector2d v1 = {1., 0.};
 
-    auto pair = new PolynomialPair(x0, x1, v0, v1, 0., 1.);
+    auto pol = CubicHermitePolynomial<2>(x0, x1, v0, v1);
 
     std::cout << "Positions:" << std::endl;
     for (double t = 0.; t <= 1.; t += 0.1) {
-        std::cout << pair->position(t).transpose() << std::endl;
+        std::cout << pol.position(t).transpose() << std::endl;
     }
 
     std::cout << "Velocities:" << std::endl;
     for (double t = 0.; t <= 1.; t += 0.1) {
-        std::cout << pair->velocity(t).transpose() << std::endl;
+        std::cout << pol.velocity(t).transpose() << std::endl;
     }
 
     return 0;
