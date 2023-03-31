6481726e51eaeede963501a6f2e8c7af61b6c499
Ioannis Tsikelis <tsikelis.i@protonmail.com>
2023-03-31 13:48:40 +0300

    Example with 3D points

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,19 @@
-#include <Eigen/Dense>
+#include "cubic_hermite_polynomial.hpp"
 
 #include <iostream>
 
-template <int D>
-class CubicHermitePolynomial {
-public:
-    using Vec = Eigen::Matrix<double, D, 1>;
-
-    CubicHermitePolynomial(const Vec& initial_position, const Vec& initial_velocity, const Vec& final_position, const Vec& final_velocity)
-    {
-        _a0 = initial_position;
-        _a1 = initial_velocity;
-        _a2 = 3. * final_position - 3. * initial_position - 2. * initial_velocity - final_velocity;
-        _a3 = -2. * final_position + 2. * initial_position + initial_velocity + final_velocity;
-    }
-
-    Eigen::Vector2d position(double t)
-    {
-        return _a0 + (_a1 * t) + (_a2 * t * t) + (_a3 * t * t * t);
-    };
-
-    Eigen::Vector2d velocity(double t)
-    {
-        return _a1 + (2 * _a2 * t) + (3 * _a3 * t * t);
-    };
-
-    Eigen::Vector2d acceleration(double t)
-    {
-        return (2 * _a2) + (6 * _a3 * t);
-    };
-
-protected:
-    Vec _a0, _a1, _a2, _a3;
-};
+using CubicHermitePolynomial2D = CubicHermitePolynomial<2>;
+using CubicHermitePolynomial3D = CubicHermitePolynomial<3>;
 
 int main()
 {
-    Eigen::Vector2d x0 = {0., 0.};
-    Eigen::Vector2d v0 = {1., 0.};
+    Eigen::Vector3d x0 = {0., 0., 0.};
+    Eigen::Vector3d v0 = {1., 0., 1.};
 
-    Eigen::Vector2d x1 = {1., 1.};
-    Eigen::Vector2d v1 = {1., 0.};
+    Eigen::Vector3d x1 = {1., 1., 0.};
+    Eigen::Vector3d v1 = {1., 1., 1.};
 
-    auto pol = CubicHermitePolynomial<2>(x0, v0, x1, v1);
+    CubicHermitePolynomial3D pol = CubicHermitePolynomial3D(x0, v0, x1, v1);
 
     std::cout << "Positions:" << std::endl;
     for (double t = 0.; t <= 1.; t += 0.1) {
