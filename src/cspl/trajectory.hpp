ad0acd475e1175767f03a3e2a5660738fda811fc
Ioannis Tsikelis <tsikelis.i@protonmail.com>
2023-04-02 16:38:25 +0300

    Minor mistake in a polynomial template

--- src/cspl/trajectory.hpp
+++ src/cspl/trajectory.hpp
@@ -21,7 +21,7 @@ namespace cspl
             auto prev_pos = _vec.back().pol.position(1); // REVIEW: Maybe store initial and last positions in polynomial object to avoid calling position(1)?
             auto prev_vel = _vec.back().pol.velocity(1);
 
-            auto pol = CubicHermitePolynomial3D(prev_pos, prev_vel, next_pos, next_vel);
+            auto pol = CubicHermitePolynomial<D>(prev_pos, prev_vel, next_pos, next_vel);
             PolynomialTimePair pair{pol, duration};
 
             _vec.push_back(pair);
