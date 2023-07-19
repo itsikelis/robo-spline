54e3a02bf04a6d16791b8c433dac99c7b230d51d
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2023-07-19 13:10:05 +0300

    Fix CMake installation

---
+++ robo_spline-config.cmake
@@ -0,0 +1,8 @@
+# these are autogenerate by cmake
+include("${CMAKE_CURRENT_LIST_DIR}/robo_spline-targets.cmake")
+
+get_target_property(robo_spline_INCLUDE_DIRS robo_spline::robo_spline INTERFACE_INCLUDE_DIRECTORIES)
+
+# robo_spline
+get_property(robo_spline_LIB_CORE TARGET robo_spline::robo_spline PROPERTY LOCATION)
+list(APPEND robo_spline_LIBRARIES ${robo_spline})
