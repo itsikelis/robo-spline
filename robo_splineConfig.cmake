352ce514e6ce3555d321029cf2687b46a2750502
Ioannis Tsikelis <tsikelis.i@protonmail.com>
2023-07-21 01:09:50 +0300

    Minor changes

--- robo_splineConfig.cmake
+++ robo_splineConfig.cmake
@@ -1,8 +1,9 @@
-# these are autogenerate by cmake
+# Copyright (c) 2023, Ioannis V. Tsikelis, University of Patras. All rights reserved.
+
+# Include auto-generated Targets file.
 include("${CMAKE_CURRENT_LIST_DIR}/robo_splineTargets.cmake")
 
 get_target_property(robo_spline_INCLUDE_DIRS robo_spline::robo_spline INTERFACE_INCLUDE_DIRECTORIES)
 
-# robo_spline
 get_property(robo_spline_LIB_CORE TARGET robo_spline::robo_spline PROPERTY LOCATION)
 list(APPEND robo_spline_LIBRARIES ${robo_spline})
